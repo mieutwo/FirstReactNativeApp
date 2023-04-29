be3a473174758034045d4984ced113b977312e5c
mieutwo <mandymieu@gmail.com>
2023-04-28 22:55:18 -0600

    Made custom text, some buttons with alert boxes.

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textStyle}>Mandy Mieu</Text>
+      <Text style={styles.textStyle}>This is react native</Text>
+      <Button title='Click Me' color='red' onPress={()=>alert("Welcome to my app")} disabled={false}></Button>
+      <Button title='OK' color='white' onPress={()=>alert('More button stuff')}></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontSize: 20,
+    color: 'red',
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    backgroundColor: "white"
+  }
 });
